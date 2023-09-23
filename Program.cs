f660a6263dc7d79fd717c054032db8c4b4371d23
vadim <vip-vadim_2010@mail.ru>
2023-09-23 16:09:47 +0300

    Добавили  начальный список в программу

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] massiv = new string[] {};
+
+string VvodPolzovatel = ReadInput("Введите команду: ");
+switch (VvodPolzovatel)
+{
+    case "1":
+        massiv = new string[] { "Hello", "2", "world", ":-)"};
+        break;
+    case "2":
+        massiv = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        massiv = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{VvodPolzovatel} - Такой команды нет");
+        break;
+}
